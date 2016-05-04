f19b33914e5ea2b0977f7df6e1ae167d7cad569e
StephenCWills <swills@gridprotectionalliance.org>
2016-05-04 13:10:02 -0400

    Fixed RenameProject.bat for new directory structure after conversion to Git.

--- RenameProject.bat
+++ RenameProject.bat
@@ -6,9 +6,9 @@ ECHO *** ProjectAlpha Rename Script ***
 ECHO.
 ECHO About to rename "%source%" to "%1", press Ctrl+C to cancel, or
 PAUSE
-.\Main\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Contributor Resources\*.*" %source% %1
-.\Main\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Main\*.*" %source% %1
-.\Main\Source\Dependencies\GSF\BRC64 /REPLACECI:%source%:%1 /RECURSIVE /EXECUTE
+.\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Build\*.*" %source% %1
+.\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Source\*.*" %source% %1
+.\Source\Dependencies\GSF\BRC64 /REPLACECI:%source%:%1 /RECURSIVE /EXECUTE
 ECHO.
 ECHO Project Rename Complete.
 ECHO.
