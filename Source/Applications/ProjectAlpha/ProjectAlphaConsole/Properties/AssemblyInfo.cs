787f11e25ab95a46630a39bb6c810eeb66d4e83b
gsfbuildbot <support@gridprotectionalliance.org>
2023-07-26 02:24:47 -0400

    ProjectAlpha: Version change for build v0.6.175.0-master.

--- Source/Applications/ProjectAlpha/ProjectAlphaConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/ProjectAlpha/ProjectAlphaConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("0.6.174.0")]
+[assembly: AssemblyVersion("0.6.175.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
